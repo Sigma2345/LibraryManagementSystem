82354f6524837b2a8b27d1bff5382ca3fdeb0fd1
Veeresh Pratap Singh <103752873+SevereasPrangphit@users.noreply.github.com>
2024-02-24 21:34:25 +0530

    Add Frontend code

---
+++ LMS/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogInComponent } from './Features/log-in/log-in.component';
+import { ProfileComponent } from './Features/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogInComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
